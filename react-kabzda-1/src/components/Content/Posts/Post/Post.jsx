9588473ae81e22f7da6af64cf9d30af11709a947
semenyuk <semenyuk_andrey@list.ru>
2020-10-01 10:58:31 +0300

    01.10.20 prokidyvanie cherez props funkcii

--- react-kabzda-1/src/components/Content/Posts/Post/Post.jsx
+++ react-kabzda-1/src/components/Content/Posts/Post/Post.jsx
@@ -3,19 +3,10 @@ import React from "react";
 
 const Post = (props) => {
 
-  let newTextItem = React.createRef();
-  let addPost = () => {
-    let text = newTextItem.current.value;
-    alert(text);
-  }
-
   return (
     <div className={s.item}>
       <img src={props.ava} id={props.id} alt={props.id} />
-      <div>
-        <input ref={newTextItem} placeholder='enter text'/>
-        <button onClick={addPost}>Send</button>
-      </div>
+      <div>{props.message}</div>
     </div>
   )
 }
--- react-kabzda-1/src/components/Content/Posts/Posts.jsx
+++ react-kabzda-1/src/components/Content/Posts/Posts.jsx
@@ -3,11 +3,21 @@ import React from "react";
 import Post from "./Post/Post";
 
 const Posts = (props) => {
-
   let postsElement = props.posts.map(p => <Post message={p.message} ava={p.ava} id={p.id}/>)
+
+  let newItem = React.createRef();
+  let addPost = () => {
+    let text = newItem.current.value;
+    props.addPost(text);
+  }
 
   return (
     <div className={s.posts}>
+      <h3>My Posts</h3>
+      <textarea ref={newItem}>/</textarea>
+      <div>
+        <button onClick={addPost}>Send</button>
+      </div>
       {postsElement}
     </div>
   )
