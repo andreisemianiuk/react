482a4acacd38dbe112035c3aa3c4cf4ac51bb8f2
MrBo88 <Mr@Bo88.io>
2020-11-20 17:20:03 +0300

    Updates

---
+++ precache-manifest.1399ff8da63b446d7b8bb0e6304318e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7557903123dc91d067e985441ae91f2",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f47e519442e0fc101065",
+    "url": "/react/static/css/3.fc0a0a07.chunk.css"
+  },
+  {
+    "revision": "78145552f93cd84a6a89",
+    "url": "/react/static/css/4.0d3c6260.chunk.css"
+  },
+  {
+    "revision": "fdea844acec685f10ee6",
+    "url": "/react/static/css/main.2f7d7c07.chunk.css"
+  },
+  {
+    "revision": "6a1d599f869e49ca3fe0",
+    "url": "/react/static/js/2.586ebbf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.586ebbf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47e519442e0fc101065",
+    "url": "/react/static/js/3.f28fd3c6.chunk.js"
+  },
+  {
+    "revision": "78145552f93cd84a6a89",
+    "url": "/react/static/js/4.0f19a608.chunk.js"
+  },
+  {
+    "revision": "fdea844acec685f10ee6",
+    "url": "/react/static/js/main.cc6c4e97.chunk.js"
+  },
+  {
+    "revision": "2a7c41f46a1d3c5e9ffc",
+    "url": "/react/static/js/runtime-main.031b8c80.js"
+  },
+  {
+    "revision": "0634a5babf36db50e415356b348e3f30",
+    "url": "/react/static/media/instagram.0634a5ba.png"
+  },
+  {
+    "revision": "303288cf0077186ace089534b80b9afe",
+    "url": "/react/static/media/preloader.303288cf.gif"
+  },
+  {
+    "revision": "c228a51f52d262f60b78c320bb8fb782",
+    "url": "/react/static/media/twitter.c228a51f.png"
+  },
+  {
+    "revision": "d5ba8abe7988d23fc3c948be8d3bbe93",
+    "url": "/react/static/media/vk.d5ba8abe.svg"
+  },
+  {
+    "revision": "cf0c5666e1db15c98b409afcacd4fcdf",
+    "url": "/react/static/media/withJob.cf0c5666.gif"
+  },
+  {
+    "revision": "ca0021e7b94699b7990ee25dd9c0cd89",
+    "url": "/react/static/media/withoutJob.ca0021e7.gif"
+  }
+]);
